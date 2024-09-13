041627debea8e992cc7218261876039748778869
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-09-13 15:40:21 -0600

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -20,11 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        {/* Layout UI */}
+        <main>{children}</main>
       </body>
     </html>
-  );
+  )
 }
