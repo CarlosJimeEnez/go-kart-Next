90a740027cf829e695ffabd04ec173b94f6dc5a1
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-09-15 01:19:14 -0600

    feat: more cars

--- src/app/ui/live-map.js
+++ src/app/ui/live-map.js
@@ -8,32 +8,34 @@ import * as THREE from 'three'
 export default function LiveMap() {
     const mountRef = useRef(null)
 
-    function createVehicle(pathPoints, maxSpeed = 1, _color){
+    function createVehicle(scene, pathPoints, maxSpeed = 1, camera, renderer){
         const vehicle = new YUKA.Vehicle(); 
 
+        const vehicleGeometry = new THREE.ConeGeometry(0.1, 0.5, 8);
+        vehicleGeometry.rotateX(Math.PI * 0.5);
+        const vehicleMaterial = new THREE.MeshNormalMaterial();
         const vehicleMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(),
-            new THREE.MeshBasicMaterial({color: _color})
+            vehicleGeometry,
+            vehicleMaterial,
         );
-
-        function sync(entity, renderComponent){
-            renderComponent.matrix.copy(entity.worldMatrix);
-        }
-        vehicle.setRenderComponent(vehicleMesh, sync)
+        vehicleMesh.matrixAutoUpdate = false;
 
         //Crear el camio del vehiculo: 
         const path = new YUKA.Path()
         path.loop = false;
-
-        //Puntos del camino: 
         for(let point of pathPoints){
             path.add(point)
         }
-        vehicle.position.copy(path_coche1.current());
-        vehicle.maxSpeed = maxSpeed;
-        vehicle.followPath(path);
-        vehicle.arriveThreshold = 0.1; // Ajustar la distancia de llegada al punto
 
+
+        vehicle.position.copy(path.current())
+        vehicle.setRenderComponent(vehicleMesh, sync)
+        function sync(entity, renderComponent){
+            renderComponent.matrix.copy(entity.worldMatrix);
+        }
+      
+
+        vehicle.maxSpeed = maxSpeed;
         const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5);
         vehicle.steering.add(followPathBehavior);
 
@@ -44,7 +46,10 @@ export default function LiveMap() {
         const entityManager = new YUKA.EntityManager();
         entityManager.add(vehicle);
 
-        return {vehicle, vehicleMesh}
+        scene.add(vehicleMesh);
+        // renderer.render(scene, camera);
+
+        return {path, entityManager, vehicleMesh}
     }
 
     function createColliderBetweenPoints(pointA, pointB, scene) {
@@ -116,48 +121,47 @@ export default function LiveMap() {
         mountRef.current.appendChild(renderer.domElement);
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.target.set(-2, 0, 3);
-
-        const vehicleGeometry = new THREE.ConeGeometry(0.1, 0.5, 8);
-        vehicleGeometry.rotateX(Math.PI * 0.5);
-        const vehicleMaterial = new THREE.MeshNormalMaterial();
-        const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
-        vehicleMesh.matrixAutoUpdate = false;
-        scene.add(vehicleMesh);
-
-        // ** --------------------------- YUKA vehicle---------------------- // 
-        const vehicle = new YUKA.Vehicle()
-        vehicle.setRenderComponent(vehicleMesh, sync)
-        function sync(entity, renderComponent) {
-            renderComponent.matrix.copy(entity.worldMatrix);
-        }
-        const path_coche1 = new YUKA.Path()
         
-        // Sectors and Paths 
-        path_coche1.add(new YUKA.Vector3(0,0,0))
-        path_coche1.add(new YUKA.Vector3(-1,0,-1))
-        path_coche1.add(new YUKA.Vector3(-2,0,-1.5))
-        path_coche1.add(new YUKA.Vector3(-3,0,-0.9))
-        path_coche1.add( new YUKA.Vector3(-3, 0, 5));
-        path_coche1.add( new YUKA.Vector3(-2, 0, 6));
-        path_coche1.add( new YUKA.Vector3(-2, 0, 6));
-        path_coche1.add( new YUKA.Vector3(-1, 0, 6));
-        
-        path_coche1.add( new YUKA.Vector3(-1, 0, -1));
-        path_coche1.add( new YUKA.Vector3(-2, 0, -1.5));
-        path_coche1.add( new YUKA.Vector3(-3, 0, -0.9));
-        path_coche1.add( new YUKA.Vector3(-3, 0, 5));
-        path_coche1.add( new YUKA.Vector3(-2, 0, 6));
-        path_coche1.add( new YUKA.Vector3(-2, 0, 6));
-        path_coche1.add( new YUKA.Vector3(-1, 0, 6));
-        path_coche1.add( new YUKA.Vector3(-1, 0, -1));
-        
-        vehicle.position.copy(path_coche1.current());
-        vehicle.maxSpeed = 1; 
+        // Sectors and Paths
+        const pathsPoints1= [
+            new YUKA.Vector3(0,0,0),
+            new YUKA.Vector3(-1,0,-1),
+            new YUKA.Vector3(-2,0,-1.5),
+            new YUKA.Vector3(-3,0,-0.9),
+            new YUKA.Vector3(-3, 0, 5),
+            new YUKA.Vector3(-2, 0, 6),
+            new YUKA.Vector3(-2, 0, 6),
+            new YUKA.Vector3(-1, 0, 6),
+            
+            new YUKA.Vector3(-1, 0, -1),
+            new YUKA.Vector3(-2, 0, -1.5),
+            new YUKA.Vector3(-3, 0, -0.9),
+            new YUKA.Vector3(-3, 0, 5),
+            new YUKA.Vector3(-2, 0, 6),
+            new YUKA.Vector3(-2, 0, 6),
+            new YUKA.Vector3(-1, 0, 6),
+            new YUKA.Vector3(-1, 0, -1),
+        ] 
 
+        
+        const { path: path1, entityManager: entityManager1, vehicleMesh: vehicleMesh1 } = createVehicle(scene,pathsPoints1, 1, camera, renderer)
+        const { path: path2, entityManager: entityManager2, vehicleMesh: vehicleMesh2 } = createVehicle(scene, pathsPoints1, 1.1, camera, renderer) 
+        const { path: path3, entityManager: entityManager3, vehicleMesh: vehicleMesh3 } = createVehicle(scene, pathsPoints1, 1.2, camera, renderer) 
+        const { path: path4, entityManager: entityManager4, vehicleMesh: vehicleMesh4 } = createVehicle(scene, pathsPoints1, 0.5, camera, renderer) 
+        const { path: path5, entityManager: entityManager5, vehicleMesh: vehicleMesh5 } = createVehicle(scene, pathsPoints1, 0.9, camera, renderer) 
+        const { path: path6, entityManager: entityManager6, vehicleMesh: vehicleMesh6 } = createVehicle(scene, pathsPoints1, 0.8, camera, renderer) 
+        const { path: path7, entityManager: entityManager7, vehicleMesh: vehicleMesh7 } = createVehicle(scene, pathsPoints1, 0.4, camera, renderer) 
+        const { path: path8, entityManager: entityManager8, vehicleMesh: vehicleMesh8 } = createVehicle(scene, pathsPoints1, 0.7, camera, renderer) 
+        const { path: path9, entityManager: entityManager9, vehicleMesh: vehicleMesh9 } = createVehicle(scene, pathsPoints1, 1.3, camera, renderer) 
+        const { path: path10, entityManager: entityManager10, vehicleMesh: vehicleMesh10 } = createVehicle(scene, pathsPoints1, 0.6, camera, renderer) 
+        const entityManagers = [entityManager1, entityManager2, entityManager3, entityManager4, entityManager5, entityManager6, entityManager7, entityManager8, entityManager9, entityManager10]
+        const vehicleMesh = [vehicleMesh1, vehicleMesh2, vehicleMesh3]
+
+        // Colliders
         const colliders = []
-        for(let i = 0; i < path_coche1._waypoints.length - 1; i++){
-            const pointA = path_coche1._waypoints[i];
-            const pointB = path_coche1._waypoints[i+1];
+        for(let i = 0; i < path1._waypoints.length - 1; i++){
+            const pointA = path1._waypoints[i];
+            const pointB = path1._waypoints[i+1];
 
             if(i < 8){
                 const collider = createColliderBetweenPoints(pointA, pointB, scene); 
@@ -165,21 +169,10 @@ export default function LiveMap() {
             }
         }
 
-        const followPathBehavior = new YUKA.FollowPathBehavior(path_coche1, 0.5);
-        vehicle.steering.add(followPathBehavior);
-
-        const onPathBehavior = new YUKA.OnPathBehavior(path_coche1);
-        onPathBehavior.radius = 0.1;
-        vehicle.steering.add(onPathBehavior);
-
-        const entityManager = new YUKA.EntityManager();
-        entityManager.add(vehicle);
-
-
         // Renderizacion del Path: 
         const position = [];
-        for(let i = 0; i < path_coche1._waypoints.length; i++) {
-            const waypoint = path_coche1._waypoints[i];
+        for(let i = 0; i < path1._waypoints.length; i++) {
+            const waypoint = path1._waypoints[i];
             position.push(waypoint.x, waypoint.y, waypoint.z);
         }
 
@@ -196,14 +189,15 @@ export default function LiveMap() {
         const animate = () => {
             requestAnimationFrame(animate);
             const delta = time.update().getDelta();
-            entityManager.update(delta)
+            entityManagers.forEach(manager => {
+                manager.update(delta);
+            });
             
             // Renderizar la escena
             controls.update()
             renderer.render(scene, camera);
 
-            const collidingColliders = checkCollisions(vehicleMesh, colliders)
-
+            const collidingColliders = checkCollisions(vehicleMesh2, colliders)
             if (collidingColliders.length > 0) {
                 console.log("El vehículo está colisionando con los siguientes colliders:", collidingColliders);
             }
